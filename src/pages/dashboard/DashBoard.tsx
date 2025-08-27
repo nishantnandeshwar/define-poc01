b26699ceecf63a6ba3225323aa292dc1c79fbf18
nishantnandeshwar <nishantnandeshwar8@gmail.com>
2025-08-27 18:01:59 +0530

    update dashboard design

--- src/pages/dashboard/DashBoard.tsx
+++ src/pages/dashboard/DashBoard.tsx
@@ -1,53 +1,143 @@
-import React from "react";
-import { useNavigate, useOutletContext } from "react-router-dom";
-import "./Dashboard.css";
+import React, { useState } from "react";
+import styles from "./DashboardNew.module.css";
 import { getReduxState } from "../../redux/store/Store";
-import { useAppDispatch } from "../../utils";
 import { logout } from "../../redux/actions/auth.action";
-
+import { useAppDispatch } from "../../utils";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { dashBoardData } from "./constant/DashBoardConstant";
 
 interface OutletContextType {
-  onLogout?: (value: boolean) => void;
-  isLogIn: boolean;
+    onLogout?: (value: boolean) => void;
+    isLogIn: boolean;
 }
 
-export default function Dashboard() {
-  const navigate = useNavigate();
-  const { onLogout, isLogIn } = useOutletContext<OutletContextType>();
 
-  const dispatch = useAppDispatch();
+const Dashboard: React.FC = () => {
+    const [openCards, setOpenCards] = useState<string[]>([]);
+    const [optionMenuOpen, setOptionMenuOpen] = useState<boolean>(false);
+    const { onLogout, isLogIn } = useOutletContext<OutletContextType>();
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
 
-  const logoutAndClearStore = () => {
-    dispatch(logout());
-    onLogout?.(false);
-    navigate("/", { replace: true });
-  }
+    const toggleCard = (id: string) => {
+        setOpenCards((prev) =>
+            prev.includes(id) ? prev.filter((cardId) => cardId !== id) : [...prev, id]
+        );
+    };
 
-  const handleMembership = () => {
-    navigate("/membership-directory");
-  };
+    function getUserName() {
+        return getReduxState()?.auth?.user?.FirstName + " " + getReduxState()?.auth?.user?.LastName
+    }
 
-  // console.log("getReduxState>>>", JSON.stringify(getReduxState()?.auth))
+    const handleOptionMenu = () => {
+        setOptionMenuOpen(prev => !prev)
+    }
 
-  function getUserName() {
-    return getReduxState()?.auth?.user?.FirstName + " " + getReduxState()?.auth?.user?.LastName
-  }
+    const logoutAndClearStore = () => {
+        dispatch(logout());
+        onLogout?.(false);
+        navigate("/", { replace: true });
+    }
 
-  return (
-    <div className="dashboard-container">
-      <div className="dashboard-card">
-        <h1>Dashboard</h1>
-        <p><strong>{getUserName()}</strong>, You are logged in ✅</p>
+    return (
+        <div className={styles.dashboardContainer}>
+            <header className={styles.header}>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <img
+                        src={require('../../assets/images/member_first_logo.png')}
+                        alt="logo"
+                        className={styles.logoImg}
+                    />
+                    <div className={styles.verticalBar}></div>
+                    <span className={styles.mrmText}>MRM</span>
+                </div>
+                <div className={styles.userOuterContainer}>
+                    <button className={styles.userLogIn} onClick={handleOptionMenu}>
+                        <img
+                            src={require('../../assets/images/user.png')}
+                            alt="user-image"
+                            className={styles.userImg}
+                        />
+                        <span className={styles.userNameStyle}>{getUserName()}</span>
+                        <img
+                            src={optionMenuOpen ? require('../../assets/images/downArrow.png') : require('../../assets/images/upArrow.png')}
+                            alt="user-image"
+                            className={styles.userImg}
+                        />
+                    </button>
+                    {
+                        optionMenuOpen &&
+                        <div className={styles.optionMenuContainer}>
+                            <div className={styles.optionHeader}>
+                                {getUserName()}
+                            </div>
+                            <div className={styles.horizontalLine}></div>
+                            <button className={styles.optionTitle} onClick={logoutAndClearStore}>
+                                <img
+                                    src={require('../../assets/images/logout.png')}
+                                    alt="user-image"
+                                    className={styles.logOutImg}
+                                />
+                                <span className={styles.logOutText}>Logout</span>
+                            </button>
+                        </div>
+                    }
 
-        <div className="dashboard-buttons">
-          <button className="btn btn-membership" onClick={handleMembership}>
-            Membership Directory
-          </button>
-          <button className="btn btn-logout" onClick={logoutAndClearStore}>
-            Logout
-          </button>
+                </div>
+            </header>
+
+            <main className={styles.body}>
+                <div className={styles.cardsContainer}>
+                    {dashBoardData.map((itemList, idx) => {
+                        const key = Object.keys(itemList)[0];
+                        const children = itemList[key as keyof typeof itemList] as {
+                            route: string;
+                            value: string;
+                        }[];
+
+                        return (
+                            <div key={idx} className={styles.card}>
+                                <div className={styles.cardHeader} onClick={() => toggleCard(key)} >
+                                    <span className={styles.headerTitle}>
+                                        {key}
+                                    </span>
+                                    {
+                                        <span className={styles.toggleIcon}>
+                                            {openCards.includes(key) ?
+                                                <img
+                                                    src={require('../../assets/images/downArrow.png')}
+                                                    alt="down-arrow-image"
+                                                    className={styles.updownArrowImg}
+                                                />
+                                                :
+                                                <img
+                                                    src={require('../../assets/images/upArrow.png')}
+                                                    alt="up-arrow-image"
+                                                    className={styles.updownArrowImg}
+                                                />
+                                            }
+                                        </span>
+                                    }
+                                </div>
+
+                                {
+                                    openCards.includes(key) && children?.length > 0 && (
+                                        <div className={styles.cardBody}>
+                                            {children.map((child, idx2) => (
+                                                <div key={idx2} className={styles.childContainer}>
+                                                    <a href={child.route}>{child.value}</a>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        );
+                    })}
+                </div>
+            </main>
         </div>
-      </div>
-    </div>
-  );
-}
+    );
+};
+
+export default Dashboard;
---
+++ src/pages/dashboard/constant/DashBoardConstant.ts
@@ -0,0 +1,65 @@
+export const dashBoardData = [
+        {
+            'COMMUNICATIONS': []
+        },
+        {
+            'CLUB ACTIVITY': []
+        },
+        {
+            'DINING': []
+        },
+        {
+            'MEMBERSHIP': [
+                { route: '/member-activities', value: 'Member Activities/Tracking' },
+                { route: '/member-batch-edit', value: 'Member Batch Edit' },
+                { route: '/member-dates-isting', value: 'Member Dates Listing' },
+                { route: '/member-directory', value: 'Member Directory' },
+                { route: '/member-export', value: 'Member Export' },
+                { route: '/member-password', value: 'Member Passwords' },
+
+                { route: '/member-activities', value: 'Member Activities/Tracking' },
+                { route: '/member-batch-edit', value: 'Member Batch Edit' },
+                { route: '/member-dates-isting', value: 'Member Dates Listing' },
+                { route: '/member-directory', value: 'Member Directory' },
+                { route: '/member-export', value: 'Member Export' },
+                { route: '/member-password', value: 'Member Passwords' },
+                { route: '/member-activities', value: 'Member Activities/Tracking' },
+                { route: '/member-batch-edit', value: 'Member Batch Edit' },
+                { route: '/member-dates-isting', value: 'Member Dates Listing' },
+                { route: '/member-directory', value: 'Member Directory' },
+                { route: '/member-export', value: 'Member Export' },
+                { route: '/member-password', value: 'Member Passwords' },
+                { route: '/member-activities', value: 'Member Activities/Tracking' },
+                { route: '/member-batch-edit', value: 'Member Batch Edit' },
+                { route: '/member-dates-isting', value: 'Member Dates Listing' },
+                { route: '/member-directory', value: 'Member Directory' },
+                { route: '/member-export', value: 'Member Export' },
+                { route: '/member-password', value: 'Member Passwords' },
+                { route: '/member-activities', value: 'Member Activities/Tracking' },
+                { route: '/member-batch-edit', value: 'Member Batch Edit' },
+                { route: '/member-dates-isting', value: 'Member Dates Listing' },
+                { route: '/member-directory', value: 'Member Directory' },
+                { route: '/member-export', value: 'Member Export' },
+                { route: '/member-password', value: 'Member Passwords' },
+            ]
+        },
+        {
+            'ACCOUNT STATEMENTS': [
+                { route: '/member-activities', value: 'Member Activities/Tracking' },
+                { route: '/member-batch-edit', value: 'Member Batch Edit' },
+                { route: '/member-dates-isting', value: 'Member Dates Listing' },
+                { route: '/member-directory', value: 'Member Directory' },
+                { route: '/member-export', value: 'Member Export' },
+                { route: '/member-password', value: 'Member Passwords' },
+                { route: '/member-activities', value: 'Member Activities/Tracking' },
+                { route: '/member-batch-edit', value: 'Member Batch Edit' },
+                { route: '/member-dates-isting', value: 'Member Dates Listing' },
+                { route: '/member-directory', value: 'Member Directory' },
+                { route: '/member-export', value: 'Member Export' },
+                { route: '/member-password', value: 'Member Passwords' },
+                { route: '/member-activities', value: 'Member Activities/Tracking' },
+                { route: '/member-batch-edit', value: 'Member Batch Edit' },
+                { route: '/member-dates-isting', value: 'Member Dates Listing' },
+            ]
+        },
+    ]
